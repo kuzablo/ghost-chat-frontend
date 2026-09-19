666c9ffd90b6348c4c37392cbba9994dddfafb82
banjoboy <kuzablo422@yandex.ru>
2026-09-19 21:07:09 +0300

    const VERSION = '2.12.2';

--- src/mainComponent/Chat.jsx
+++ src/mainComponent/Chat.jsx
@@ -17,7 +17,7 @@ import {
 } from './utils';
 import './Chat.css';
 
-const VERSION = '2.12.1';
+const VERSION = '2.12.2';
 const API_URL = 'https://api.banjoboy420.ru';
 const WS_URL = 'wss://api.banjoboy420.ru';
 
