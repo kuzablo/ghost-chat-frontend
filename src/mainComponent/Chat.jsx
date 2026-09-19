2799e3db7d1ac16e51f2fb4c98d0d401a2fa1ce8
banjoboy <kuzablo422@yandex.ru>
2026-09-19 23:43:24 +0300

    fix: friends list scroll on mobile (v2.13.3)

--- src/mainComponent/Chat.jsx
+++ src/mainComponent/Chat.jsx
@@ -19,7 +19,7 @@ import {
 import './Chat.css';
 import './Chat.mobile.css';
 
-const VERSION = '2.13.2';
+const VERSION = '2.13.3';
 const API_URL = 'https://api.banjoboy420.ru';
 const WS_URL = 'wss://api.banjoboy420.ru';
 
